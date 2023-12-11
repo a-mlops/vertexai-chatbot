967b8241ee79c02814f697b3113f02979eb4a80d
A Mlops <aleks.khachikian@gmail.com>
2023-12-11 23:41:38 +0100

    Search engine

---
+++ config.py
@@ -0,0 +1,2 @@
+project_id = "chatbot-407717"  
+region = "europe-west3"  
---
+++ main.py
@@ -0,0 +1,7 @@
+import logging
+from drivers import main
+
+logging.basicConfig(level=logging.ERROR) 
+
+if __name__ == '__main__':
+    main()
